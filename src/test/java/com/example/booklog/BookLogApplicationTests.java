995ffe9b5bd15428872f7031f70083c00e73c7f4
GrigoriiPakhomov <pakhomov-86@list.ru>
2025-12-06 02:20:29 +0800

    Initial project setup: added Spring Boot skeleton

---
+++ src/test/java/com/example/booklog/BookLogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.booklog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookLogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
