a15e26ebf62e6d94cba7296c2e401a9a4e196844
GrigoriiPakhomov <pakhomov-86@list.ru>
2025-12-06 17:31:13 +0800

    added Book model and basic controller

---
+++ src/main/java/com/example/booklog/model/Book.java
@@ -0,0 +1,48 @@
+package com.example.booklog.model;
+
+public class Book {
+
+    //Уникальный Id книги
+    private Long id;
+
+    //Название книги
+    private String title;
+
+    //Автор книги
+    private String author;
+
+    //Статус прочтения
+    private boolean read;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public boolean isRead() {
+        return read;
+    }
+
+    public void setRead(boolean read) {
+        this.read = read;
+    }
+}
